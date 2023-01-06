4eb8cc51b706d9d1813e45f54fa5ae284d50ca8b
Nguyen Quang THO <thonqFX17090@funix.edu.vn>
2023-01-06 23:38:01 +0700

    pro

--- controller/product.js
+++ controller/product.js
@@ -2,9 +2,9 @@ const Product = require("../model/product");
 
 exports.getProduct = async (req, res, next) => {
   try {
-    const product = await Product.find();
+    //const product = await Product.find();
 
-    res.status(200).json(product);
+    res.status(200).json("product");
   } catch (err) {
     next(err);
   }
